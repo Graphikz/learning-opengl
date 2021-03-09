01db82d8173de681cede4db7279d656c4473ac96
Ricky Chon <rickychon99@gmail.com>
2021-03-09 13:10:52 -0500

    Add Glitter OpenGL boilerplate

---
+++ Glitter/CMakeFiles/Glitter.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/glad/src/glad.c" "/home/rickyc0626/learning-opengl/Glitter/CMakeFiles/Glitter.dir/Glitter/Vendor/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_INCLUDE_NONE"
+  "PROJECT_SOURCE_DIR=\"/home/rickyc0626/learning-opengl/Glitter\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "Glitter/Headers"
+  "Glitter/Vendor/assimp/include"
+  "Glitter/Vendor/bullet/src"
+  "Glitter/Vendor/glad/include"
+  "Glitter/Vendor/glfw/include"
+  "Glitter/Vendor/glm"
+  "Glitter/Vendor/stb"
+  "Glitter/Vendor/assimp/code/../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Sources/main.cpp" "/home/rickyc0626/learning-opengl/Glitter/CMakeFiles/Glitter.dir/Glitter/Sources/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  "PROJECT_SOURCE_DIR=\"/home/rickyc0626/learning-opengl/Glitter\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Glitter/Headers"
+  "Glitter/Vendor/assimp/include"
+  "Glitter/Vendor/bullet/src"
+  "Glitter/Vendor/glad/include"
+  "Glitter/Vendor/glfw/include"
+  "Glitter/Vendor/glm"
+  "Glitter/Vendor/stb"
+  "Glitter/Vendor/assimp/code/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/assimp/code/CMakeFiles/assimp.dir/DependInfo.cmake"
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/bullet/src/BulletDynamics/CMakeFiles/BulletDynamics.dir/DependInfo.cmake"
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/bullet/src/BulletCollision/CMakeFiles/BulletCollision.dir/DependInfo.cmake"
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/bullet/src/LinearMath/CMakeFiles/LinearMath.dir/DependInfo.cmake"
+  "/home/rickyc0626/learning-opengl/Glitter/Glitter/Vendor/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
